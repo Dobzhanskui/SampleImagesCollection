ec8d9e612931277757d0278f54940983284f6001
Volodymyr Dobzhanskui <Volodymyr.Dobzhanskui@gmail.com>
2019-02-27 12:50:57 +0200

    update Version 1.0 (SampleImagesCollection)

---
+++ SampleImagesCollection/Helpers/ImageEdit.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SampleMVVMWPF.Helpers
+{
+    public class ImageEdit : INotifyPropertyChanged
+    {
+        #region Members
+
+        private DrawingImage m_bitImage;
+
+        #endregion // Members
+
+        #region Properties
+
+        public DrawingImage BitImage
+        {
+            get => m_bitImage;
+            set
+            {
+                if (m_bitImage != value)
+                {
+                    m_bitImage = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+
+        #endregion // Properties
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion // INotifyPropertyChanged
+    }
+}
---
+++ SampleImagesCollection/Helpers/ImageEditViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace SampleMVVMWPF.Helpers
+{
+    public class ImageEditViewModel : INotifyPropertyChanged
+    {
+        #region Members
+
+        private string m_title;
+        private ImageEdit m_imageEdit;
+
+        #endregion // Members
+
+        #region Properties
+
+        public string Title
+        {
+            get => m_title;
+            set
+            {
+                if (m_title != value)
+                {
+                    m_title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+
+        #endregion // Properties
+
+        #region Constructor
+
+        public ImageEditViewModel(ImageEdit image)
+        {
+            m_imageEdit = image;
+        }
+
+        #endregion //Constructor
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion // INotifyPropertyChanged
+    }
+}
